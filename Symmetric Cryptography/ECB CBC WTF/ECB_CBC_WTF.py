cc92b371f46d60b33f112d02119052be2efb6997
Pauln0503 <nphu2300@gmail.com>
2025-08-05 13:04:42 +0700

    feat: Update ecb_cbc_wtf.py

--- Symmetric Cryptography/ECB CBC WTF/ECB_CBC_WTF.py
+++ Symmetric Cryptography/ECB CBC WTF/ECB_CBC_WTF.py
@@ -1,16 +1,38 @@
-import os
-iv = os.urandom(16)
-print(iv.hex())
-
-ok = "db67a2ff2c5c9433b4029b74f0f79c1a  2732e98f18ae233695005ba1ce5041749d792ed205c1efb7c316971e2a7a8024"
-
-ciphertext = iv.hex() + ok
-print(ok)
-
-db67a2ff2c5c9433b4029b74f0f79c1a
-b815db8f5833ef00d760c4418594f72f78069fbf29ca7c07a25f7a80ef716009
+import httpx
+from pwn import xor
 
 
-'2732e98f18ae233695005ba1ce504174    9d792ed205c1efb7c316971e2a7a8024'
+BASE_URL = "http://aes.cryptohack.org/ecbcbcwtf"
 
-flag = 'crypto{3cb_5uck5_4v01d_17_!!!!!}'
+
+def fetch_ciphertext() -> str:
+    url = f"{BASE_URL}/encrypt_flag/"
+    response = httpx.get(url)
+    response.raise_for_status()
+    return response.json()["ciphertext"]
+
+
+def decrypt_ecb_block(ciphertext_hex: str) -> bytes:
+    url = f"{BASE_URL}/decrypt/{ciphertext_hex}/"
+    response = httpx.get(url)
+    response.raise_for_status()
+    pt_hex = response.json()["plaintext"]
+    return bytes.fromhex(pt_hex)
+
+
+def decrypt_flag(ciphertext_hex: str) -> str:
+    iv = bytes.fromhex(ciphertext_hex[:32])
+    ct_blocks = [ciphertext_hex[32:64], ciphertext_hex[64:]]  # 2 next block
+
+    pt_blocks = [decrypt_ecb_block(block) for block in ct_blocks]
+
+    pt1 = xor(iv, pt_blocks[0]).decode()
+    pt2 = xor(bytes.fromhex(ct_blocks[0]), pt_blocks[1]).decode()
+
+    return pt1 + pt2
+
+
+if __name__ == "__main__":
+    ciphertext = fetch_ciphertext()
+    flag = decrypt_flag(ciphertext)
+    print(f"Flag: {flag}")
